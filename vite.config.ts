3d9d439244fde0b3ab3db344a0156e73587dc795
Marius Avram <me@mariusavram.com>
2025-03-12 14:14:17 +0100

    Add basic ssl plugin for local https development

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    basicSsl({ name: "test", domains: ["*.custom.com"] }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  server: {
+    port: 3000,
+    https: true,
+  },
+});
